0d9d290122d17aa68243e65c850ed4caf557ac40
khanhbuan <namkhanhnc240803@gmail.com>
2024-07-17 17:42:39 +0700

    initial commit

--- src/data/components/lsp.py
+++ src/data/components/lsp.py
@@ -1,15 +1,30 @@
 import scipy.io
 import os
+from PIL import Image
 
 def read_data_file(root_dir):
     arr = os.listdir(os.path.join(root_dir, 'images'))
     arr.sort()
     return arr
 
-def read_mat_file(root_dir):
+def read_mat_file(root_dir, img_list):
     mat = scipy.io.loadmat(os.path.join(root_dir, 'joints.mat'))['joints']
-    print(mat.shape)
+    kpts = mat.transpose([2, 0, 1])
+    
+    centers =[]
+    for idx in range(kpts.shape[0]):
+        im = Image.open(os.path.join(root_dir, 'images', img_list[idx]))
+        w, h = im.size[0], im.size[1]
+        
+        center_x = (kpts[idx][kpts[idx][:, 0] < w, 0].max() +
+                    kpts[idx][kpts[idx][:, 0] > 0, 0].min()) / 2
+        center_y = (kpts[idx][kpts[idx][:, 1] < h, 1].max() +
+                    kpts[idx][kpts[idx][:, 1] > 0, 1].min()) / 2
+        centers.append([center_x, center_y])
 
-if __name__ == "__main__":
-    arr = read_mat_file(root_dir="./data/lsp")
-    print(arr)
+    return kpts, centers
+
+
+
+# if __name__ == "__main__":
+    
